8fbbb0c567cc0ebece3e33727b8c513a7b1f9d7c
JoeWeakliam <10351256@mydbs.ie>
2017-05-14 21:38:53 +0100

    Final submission for CA5

--- CA5/list_functions.py
+++ CA5/list_functions.py
@@ -55,19 +55,19 @@ def divide3(first, second):
 
 print divide3([2,4,6], [1,2,0,4])
 
-#5 Exponential function
+#5 Variations on exponential function
 
-#find exponentials of a list of numbers using Map
+#find exponentials for a list of numbers and exponential values using Map
 def expo(values, exponent):
     return map(lambda x, y: x**y, values, exponent) 
 
 print expo([1,2,3,4], [2,4,0,1])
 
-#find exponentials of a list of numbers
+#find exponentials of a list of numbers where a single exponential is applied using List Comprehension
 def expo2(values, exponent):
-    return map(lambda x, y: x**y, values, exponent) 
+    return [ (x**exponent) for x in values ]
 
-#print expo2([1,2,3,4], 2)
+print expo2([1,3,5,7], 3)
 
 #6 Variations on square function
 
@@ -79,10 +79,10 @@ def square(values):
 print square([2,0,3,4])
 
 #find the square of a list using List Comprehension
-Squares = [2,3,8,4]
 def square2(list):
 	return [ (x**2) for x in list ]
 
+Squares = [2,3,8,4]
 print square2(Squares)
 
 #7 Minimum value 
@@ -93,15 +93,15 @@ def min(values):
 
 print min([5,34,5,78,3.5,125])
 
-#8 Use list comprehension to record rolls where all 3 dice show the same number
+#8 Use List Comprehension to record all unique dice rolls where the total number is 14
 
 dice1 = [1,2,3,4,5,6]
 dice2 = [1,2,3,4,5,6]
 dice3 = [1,2,3,4,5,6]
-triplets = [ (x,y,z) for x in dice1 for y in dice2 for z in dice3 if x == y and x == z]
-print triplets
+total_14 = [ (x,y,z) for x in dice1 for y in dice2 for z in dice3 if x+y+z == 14]
+print total_14
 
-#9 Use generator function to return strings in a list that contain a specific substring
+#9 Use Generator Function to return strings in a list that contain a specific substring
 
 def contains(words_list):
     counter = 0
@@ -113,10 +113,20 @@ def contains(words_list):
 
 at_words = contains(['at', 'hat', 'mouse', 'car', 'bat', 'chat', 'cow'])
 for at_word in at_words:
-    print at_word,		
+    print at_word,
+print	
 
+#10a Use Filter to return even values in a list
 
+def is_even(values):
+    return filter(lambda x: x%2==0, values)
 
+print is_even([6,8,11,16])
 
+#10b Use List Comprehension to return only those rolls of two dice that generate an even number less than 8
 
+dice1 = [1,2,3,4,5,6]
+dice2 = [1,2,3,4,5,6]
+even_values = [ (x,y) for x in dice1 for y in dice2 if (x+y)%2 == 0 and (x+y) <= 8 ] 
 
+print even_values
